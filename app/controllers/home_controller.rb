81b1b9e0aa48392ff97c63078220816bbab79d74
Oana Sipos <oana.sipos@up-nxt.com>
2015-08-05 22:15:44 +0200

    Add new questions and new answers.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,15 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def questions
+    redirect_to root_path
+  end
+
+  def question
+  end
+
+  def answers
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
+
+  post '/questions' => 'home#questions'
+  get '/questions/:id' => 'home#question'
+
+  post '/answers' => 'home#answers'
 end
